05b3af402162236a05d8112c0a7f39978f290d1c
bwernick <bradley@LAPTOP-TPN2RC0C.localdomain>
2019-11-11 16:49:40 -0800

    removed flask-fontawesome reliance. using CDN instead

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, url_for
 
-# # pip install Flask-FontAwesome
-from flask_fontawesome import FontAwesome
-
 app = Flask(__name__)
-fa = FontAwesome(app)
 
 @app.route('/')
 def home():
