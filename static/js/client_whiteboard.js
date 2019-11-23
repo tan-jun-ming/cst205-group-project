3a22a8557dc27d195588664e794a79c83ecd0e0b
Salihullah <salihabid56@yahoo.com>
2019-11-22 16:28:45 -0800

    fixed color toolbar changing color

--- static/js/client_whiteboard.js
+++ static/js/client_whiteboard.js
@@ -6,11 +6,14 @@ window.onload = function() {
     var brush_size_1 = document.getElementById("brush_size_1");
     var brush_size_2 = document.getElementById("brush_size_2");
     var brush_size_3 = document.getElementById("brush_size_3");
+   	var colorToolbar = document.getElementById("colorToolbar");
+ 
     var line_width = 1;
     var stroke_style = "#000";
+    var lastColor = "black";
 
     //TODO : Get this working
-    // brush.onclick = function(){ stroke_style = "#000"; };
+    // brush.onclick = function(){ stroke_style = "#ffee33"; };
     // eraser.onclick = function(){ stroke_style = "#fff"; };
     // brush_size_1.onclick = function(){ line_width = 1; };
     // brush_size_2.onclick = function(){ line_width = 3; };
@@ -85,12 +88,25 @@ window.onload = function() {
 			this.started = false;
 		}
 	};
+
+	function onColorClick(color) {
+    // Start a new path to begin drawing in a new color.
+    	// ctx.beginPath();
+    	// ctx.closePath();
+    	// Select the new color.
+    	stroke_style = color;
+    }
 	
 	// Touch Events
 	myCanvas.addEventListener('touchstart', draw.start, false);
 	myCanvas.addEventListener('touchend', draw.end, false);
 	myCanvas.addEventListener('touchmove', draw.move, false);
-	
+
+	// Color toolabar click
+	$('#colorToolbar div').click(function(evt) {
+	console.log(evt) 
+     onColorClick(evt.target.id)});
+
 	// Disable Page Move
 	document.body.addEventListener('touchmove',function(evt){
 		evt.preventDefault();
