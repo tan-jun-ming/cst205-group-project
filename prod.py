745eb95a8534f651a8e3071bee722408ee68ccd6
tan-jun-ming <jtan@csumb.edu>
2019-12-09 13:51:37 -0800

    prod file

--- prod.py
+++ prod.py
@@ -1,6 +1,6 @@
-from main import app
+import asyncio
+from main import app, loop`
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
 
-loop = asyncio.get_event_loop()
 loop.run_until_complete(serve(app, Config()))
