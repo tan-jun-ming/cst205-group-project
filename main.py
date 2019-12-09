db9cb8a160b76b15a20ce45f3f90c8eebf354fd2
tan-jun-ming <jtan@csumb.edu>
2019-12-09 06:05:00 -0800

    I think that's everything

--- main.py
+++ main.py
@@ -1,10 +1,208 @@
-from quart import Quart, render_template, request
-import json, requests
+# Quart needs Python 3.7 and a library called Hypercorn to deploy
+from quart import Quart, websocket, copy_current_websocket_context, render_template, request, redirect, url_for
+import asyncio
+import random
+import json
 
+loop = asyncio.get_event_loop()
 app = Quart(__name__)
 
-@app.route("/")
-def hello():
-    return render_template("index.html")
+# Define connection and room dicts.
+connections = {}
+rooms = {}
 
-app.run()
+@app.route("/")
+async def index():
+    return await render_template("index.html")
+
+@app.route("/favicon.ico")
+async def favicon():
+    return redirect(url_for("static", filename="img/favicon.ico"))
+
+@app.route("/<room_id>")
+async def custom_room(room_id):
+    return await render_template("index.html")
+
+room_chars = "23456789ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz"
+
+@app.route("/initialize", methods=["POST"])
+async def initialize_room():
+    try:
+        data = await request.get_data()
+        data = decode_message(data)
+        for i in data:
+            i = i.split(",")
+            i = [int(i[0]), int(i[1])]
+    except:
+        abort(400)
+
+
+    new_room_id = None
+    while not new_room_id or new_room_id in rooms:
+        new_room_id = "".join(random.choices(room_chars, k=random.randint(5,7)))
+
+    create_room(new_room_id, data)
+
+    return new_room_id
+
+def encode_message(msg):
+    return json.dumps(msg)
+
+def decode_message(msg):
+    return json.loads(msg)
+
+def create_room(room_id, data={}):
+    if not room_id in rooms:
+        rooms[room_id] = Room(room_id, data)
+        loop.create_task(rooms[room_id].run_loop(.02))
+    
+    return rooms[room_id]
+
+class MemberState:
+    def __init__(self):
+        self.color = 1
+        self.x = 0
+        self.y = 0
+
+        # Set nickname to Anonymous
+        # (Maybe follow google and add animal names as well? Would have to make sure they are unique per room.)
+        self.nickname = "Anonymous Banana"
+    
+    def to_dict(self):
+        return {"x": self.x, "y": self.y, "color": self.color, "nick": self.nickname}
+
+
+class Room:
+    def __init__(self, room_id, pixels={}):
+        self.name = room_id
+        self.members = {}
+        self.pixels = pixels
+
+        self.changed_states = {}
+    
+    def push_new_state(self, new_data, ws):
+        clear = False
+        new_pixels = dict(new_data["pixels"])
+
+        if new_data["clear"]:
+            self.pixels = {}
+            clear = True
+        else:
+            try:
+                for i in new_pixels:
+                    i = i.split(",")
+                    i = [int(i[0]), int(i[1])]
+            except:
+                return
+
+            self.pixels.update(new_pixels)
+
+        modified = False
+        if new_data["x"] != None:
+            self.members[ws].x = new_data["x"]
+            modified = True
+        if new_data["y"] != None:
+            self.members[ws].y = new_data["y"]
+            modified = True
+        if new_data["color"] != None:
+            self.members[ws].color = new_data["color"]
+            modified = True
+        if new_data["nick"] != None:
+            self.members[ws].nickname = new_data["nick"]
+            modified = True
+        
+        if modified or new_pixels or clear:
+            self.changed_states[ws] = {
+                "pixels":{**self.changed_states.get(ws, {}).get("pixels", {}), **new_pixels},
+                "clear": clear,
+                **self.members[ws].to_dict()
+                }
+
+    async def run_loop(self, interval):
+        while True:
+            await asyncio.sleep(interval)
+            await self.broadcast()
+
+    async def broadcast(self):
+        sent2 = False
+        for member in self.members:
+            pixels = {}
+            users = []
+            clear = False
+
+            send = False
+            for m in self.members:
+                if m != member:
+                    pixels.update(self.changed_states.get(m, {}).get("pixels", {}))
+                    users.append(self.members[m].to_dict())
+                    if self.changed_states.get(m, {}).get("clear", False):
+                        clear = True
+
+                    if m in self.changed_states:
+                        send = True
+
+            if pixels or send or clear:
+                sent2 = True
+                data = {
+                    "users": users,
+                    "pixels": pixels,
+                    "clear": clear
+                }
+                data = encode_message(data)
+                await member.send(data)
+
+        if sent2:
+            self.changed_states = {}
+
+    async def new_member(self, ws):
+        self.members[ws] = MemberState()
+        self.changed_states[ws] = {
+            "pixels":{},
+            "clear": False,
+            **self.members[ws].to_dict()
+        }
+        payload = {
+            "users": [self.members[m].to_dict() for m in self.members if m != ws],
+            "pixels": self.pixels,
+            "clear": False
+        }
+
+        await ws.send(encode_message(payload))
+
+
+async def receiving():
+    while True:
+        # Receives data from each websocket and does nothing with it (for now)
+        data = await websocket.receive()
+        try:
+            data = decode_message(data)
+        except:
+            continue
+
+        obj = websocket._get_current_object()
+        if obj in connections:
+            room = rooms[connections[obj]]
+            room.push_new_state(data, obj)
+
+@app.websocket('/<room_id>')
+async def ws(room_id):
+    obj = websocket._get_current_object()
+
+    # Put connection in a dict
+    connections[obj] = room_id
+
+    room = create_room(room_id)
+
+    await room.new_member(obj)
+
+    # Tie receiving/sending loops to the connection
+    # Not sure if we need both of these if we only want to receive from these objects.
+    consumer = asyncio.ensure_future(copy_current_websocket_context(receiving)(),)
+    try:
+        await asyncio.gather(consumer)
+    finally:
+        consumer.cancel()
+        rooms[room_id].members.pop(obj)
+
+
+app.run(loop=loop)
--- static/js/client_whiteboard.js
+++ static/js/client_whiteboard.js
@@ -1,122 +1,502 @@
-window.onload = function() {
-	var myCanvas = document.getElementById("myCanvas");
-	var ctx = myCanvas.getContext("2d");
-    var eraser = document.getElementById("eraser");
-    var brush = document.getElementById("brush");
-    var brush_size_1 = document.getElementById("brush_size_1");
-    var brush_size_2 = document.getElementById("brush_size_2");
-    var brush_size_3 = document.getElementById("brush_size_3");
-   	var colorToolbar = document.getElementById("colorToolbar");
+let circles = {
+	10: calculate_circle(10),
+	20: calculate_circle(20),
+	40: calculate_circle(40),
+}
+
+function calculate_circle(width){
+	let ret = []
+	
+	let x = 0;
+	let y = 0;
+
+	let r = Math.floor(width/2);
+	let r2 = r * r;
+	let area = r2 << 2;
+	let rr = r << 1;
+	
+	for (let i = 0; i < area; i++)
+	{
+		let tx = (i % rr) - r;
+		let ty = (i / rr) - r;
+	
+		if (tx * tx + ty * ty <= r2)
+			ret.push([Math.floor(x + tx), Math.floor(y + ty)]);
+	}
+
+	return ret;
+}
+
+function calculate_line(x1, y1, x2, y2, width, color){
+	// adapted from http://www.javascriptteacher.com/bresenham-line-drawing-algorithm.html
+	
+    let ret = {};
+	
+	function pixel(x, y){
+		for (i of circles[width]){
+			ret[`${i[0] + x},${i[1] + y}`] = color;
+		}
+	}
+    // Iterators, counters required by algorithm
+    let x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+
+    // Calculate line deltas
+    dx = x2 - x1;
+    dy = y2 - y1;
+
+    // Create a positive copy of deltas (makes iterating easier)
+    dx1 = Math.abs(dx);
+    dy1 = Math.abs(dy);
+
+    // Calculate error intervals for both axis
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+
+    // The line is X-axis dominant
+    if (dy1 <= dx1) {
+
+        // Line is drawn left to right
+        if (dx >= 0) {
+            x = x1; y = y1; xe = x2;
+        } else { // Line is drawn right to left (swap ends)
+            x = x2; y = y2; xe = x1;
+        }
+
+        pixel(x, y); // Draw first pixel
+
+        // Rasterize the line
+        for (i = 0; x < xe; i++) {
+            x = x + 1;
+
+            // Deal with octants...
+            if (px < 0) {
+                px = px + 2 * dy1;
+            } else {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                    y = y + 1;
+                } else {
+                    y = y - 1;
+                }
+                px = px + 2 * (dy1 - dx1);
+            }
+
+            // Draw pixel from line span at currently rasterized position
+            pixel(x, y);
+        }
+
+    } else { // The line is Y-axis dominant
+
+        // Line is drawn bottom to top
+        if (dy >= 0) {
+            x = x1; y = y1; ye = y2;
+        } else { // Line is drawn top to bottom
+            x = x2; y = y2; ye = y1;
+        }
+
+        pixel(x, y); // Draw first pixel
+
+        // Rasterize the line
+        for (i = 0; y < ye; i++) {
+            y = y + 1;
+
+            // Deal with octants...
+            if (py <= 0) {
+                py = py + 2 * dx1;
+            } else {
+                if ((dx < 0 && dy<0) || (dx > 0 && dy > 0)) {
+                    x = x + 1;
+                } else {
+                    x = x - 1;
+                }
+                py = py + 2 * (dx1 - dy1);
+            }
+
+            // Draw pixel from line span at currently rasterized position
+            pixel(x, y);
+        }
+	}
+	return ret
+ }
  
-    var line_width = 2;
-    var stroke_style = "#000";
-    var lastColor = "black";
+let payload = {};
+reset_payload();
 
-    //TODO : Get this working
-    // brush.onclick = function(){ stroke_style = "#ffee33"; };
-    // eraser.onclick = function(){ stroke_style = "#fff"; };
-    brush_size_1.onclick = function(){ ctx.lineWidth = 2; };
-    brush_size_2.onclick = function(){ ctx.lineWidth = 4; };
-    brush_size_3.onclick = function(){ ctx.lineWidth = 6; };
-    // eraser.onclick = function(){
-    // 	ctx.clearRect(0,0,myCanvas.width, myCanvas.height);
-    // }
+function reset_payload(){
+	payload = {x: null, y: null, pixels: [], color: null, nick: null, clear: null}
+}
 
+let colors = [
+	[255, 255, 255],
+	[0, 0, 0],
+	[244, 67, 54],
+	[103, 58, 183],
+	[63, 81, 181],
+	[33, 150, 243],
+	[0, 150, 136],
+	[76, 175, 80],
+	[139, 195, 74],
+	[255, 235, 59],
+	[255, 152, 0],
+	[255, 87, 34],
+	[96, 125, 139],
+]
+
+let entire_canvas = {}
+
+animals = [
+	"Alligator",
+	"Anteater",
+	"Armadillo",
+	"Auroch",
+	"Axolotl",
+	"Badger",
+	"Banana",
+	"Bat",
+	"Bear",
+	"Beaver",
+	"Buffalo",
+	"Camel",
+	"Capybara",
+	"Chameleon",
+	"Cheetah",
+	"Chinchilla",
+	"Chipmunk",
+	"Chupacabra",
+	"Cormorant",
+	"Coyote",
+	"Crow",
+	"Dingo",
+	"Dinosaur",
+	"Dog",
+	"Dolphin",
+	"Duck",
+	"Elephant",
+	"Ferret",
+	"Fox",
+	"Frog",
+	"Giraffe",
+	"Gopher",
+	"Grizzly",
+	"Hedgehog",
+	"Hippo",
+	"Hyena",
+	"Ibex",
+	"Ifrit",
+	"Iguana",
+	"Jackal",
+	"Kangaroo",
+	"Koala",
+	"Kiwi",
+	"Kraken",
+	"Lemur",
+	"Leopard",
+	"Liger",
+	"Lion",
+	"Llama",
+	"Loris",
+	"Manatee",
+	"Mink",
+	"Monkey",
+	"Moose",
+	"Narwhal",
+	"Orangutan",
+	"Otter",
+	"Panda",
+	"Penguin",
+	"Platypus",
+	"Pumpkin",
+	"Python",
+	"Quagga",
+	"Rabbit",
+	"Raccoon",
+	"Rhino",
+	"Sheep",
+	"Shrew",
+	"Skunk",
+	"Squirrel",
+	"Tiger",
+	"Turtle",
+	"Walrus",
+	"Wolf",
+	"Wolverine",
+	"Wombat"
+]
+
+let nickname = "Anonymous " + animals[Math.floor(Math.random() * animals.length)]
+
+window.onload = function() {
+	let myCanvas = document.getElementById("myCanvas");
+	let ctx = myCanvas.getContext("2d");
+
+    let brush_size_1 = document.getElementById("brush_size_1");
+    let brush_size_2 = document.getElementById("brush_size_2");
+	let brush_size_3 = document.getElementById("brush_size_3");
+	
+	let uri = `${window.location.hostname}:${window.location.port}`
+
+	let room_id = window.location.pathname.substr(1);
+
+	if (room_id == ""){
+		room_id = null;
+	}
+
+	if (room_id){
+		let websocket = new WebSocket("ws://" + uri + "/" + room_id);
+
+		websocket.onmessage = function(event){
+			data = JSON.parse(event.data);
+	
+			if (data.clear){
+				clear_canvas();
+			} else {
+				new_pixels = {}
+				for (c of Object.keys(data.pixels)){
+					i = c.split(",").map(Number);
+		
+					let c2 = coords_to_corner(...i)
+					let k = `${c2[0]},${c2[1]}`
+		
+					new_pixels[k] = data.pixels[c]
+				}
+				place_pixels(new_pixels, true);
+			}
+	
+			$(".brush").remove()
+	
+			for (i of data.users){
+				color = colors[i.color]
+				brush = $("<i class='material-icons' style='font-size:5em;margin-right:-0.5em'>brush</i>"
+				).css("color", `RGB(${color[0]}, ${color[1]}, ${color[2]})`);
+				name_card = $("<span></span>").text(i.nick).css("padding-top", "3em");
+							
+				cc = coords_to_corner(i.x, i.y)
+				userdiv = $("<div class='brush'></div>").append(brush).append(name_card).css("left", cc[0]).css("top", cc[1]);
+				$("body").append(userdiv);
+	
+			}
+		}
+	
+		websocket.onopen = function ws_push(event) {
+			if (payload.x != null || payload.y != null || payload.pixels.size || payload.color != null || payload.nick != null || payload.clear != null) {
+				websocket.send(JSON.stringify(payload));
+				reset_payload()
+			}
+			setTimeout(ws_push, 20)
+	
+		}
+
+	} else {
+		$("#nickname-form").remove();
+		$("#nickname-feedback").text("Nicknames are disabled in private rooms. Share this room to enable it!");
+	}
+
+
+    let line_width = 10;
+	let stroke_style = 1;
+
+	payload.nick = nickname
+
+    brush_size_1.onclick = function(){ ctx.lineWidth = 10; };
+    brush_size_2.onclick = function(){ ctx.lineWidth = 20; };
+	brush_size_3.onclick = function(){ ctx.lineWidth = 40; };
+	
     // Fill Window Width and Height
     myCanvas.width = window.innerWidth;
-    myCanvas.height = window.innerHeight;
+    myCanvas.height = window.innerHeight - $(".toolbar").height();
 	
-	// Set Background Color
-	ctx.fillStyle="#fff";
+	function coords_to_center(x, y){
+		return [Math.floor(x - myCanvas.width/2), Math.floor(y - myCanvas.height/2)]
+	}
 	
+	function coords_to_corner(x, y){
+		return [Math.floor(x + myCanvas.width/2), Math.floor(y + myCanvas.height/2)]
+	}
+
+	for (i = 0; i < colors.length; i++){
+		color = colors[i]
+		toolbar_item = $("<div></div>").addClass("toolbar-item").attr("data-color", i).css("background", `RGB(${color[0]}, ${color[1]}, ${color[2]})`);
+		$("#colorToolbar").append(toolbar_item);
+	}
+
     // Mouse Event Handlers
 	if(myCanvas){
-		var isDown = false;
-		var canvasX, canvasY;
+		let isDown = false;
 		ctx.lineWidth = line_width;
 		
 		$(myCanvas)
 		.mousedown(function(e){
 			isDown = true;
-			//ctx.beginPath();
-			canvasX = e.pageX - myCanvas.offsetLeft;
-			canvasY = e.pageY - myCanvas.offsetTop;
-			ctx.moveTo(canvasX, canvasY);
+			currX = e.pageX - myCanvas.offsetLeft;
+			currY = e.pageY - myCanvas.offsetTop;
+
+			c = coords_to_center(currX, currY)
+			payload.x = c[0]
+			payload.y = c[1]
+
+			place_pixels(calculate_line(currX, currY, currX, currY, ctx.lineWidth, stroke_style), false)
 		})
 		.mousemove(function(e){
+			currX = e.pageX - myCanvas.offsetLeft;
+			currY = e.pageY - myCanvas.offsetTop;
+			
+			c = coords_to_center(currX, currY)
+			payload.x = c[0]
+			payload.y = c[1]
+
 			if(isDown !== false) {
-				canvasX = e.pageX - myCanvas.offsetLeft;
-				canvasY = e.pageY - myCanvas.offsetTop;
-				//ctx.lineTo(canvasX, canvasY);
-				var data = new Uint8ClampedArray();
-				data = Uint8ClampedArray.from([0,0,0,255]);
-				var imageData = new ImageData(data, 1, 1);
-				ctx.putImageData(imageData, canvasX, canvasY); 
-				//ctx.strokeStyle = stroke_style;
-				//ctx.stroke();
+
+				prevX = currX - e.originalEvent.movementX
+				prevY = currY - e.originalEvent.movementY
+				
+				place_pixels(calculate_line(prevX, prevY, currX, currY, ctx.lineWidth, stroke_style), false)
+
 			}
 		})
 		.mouseup(function(e){
 			isDown = false;
 			//ctx.closePath();
 		});
+	
+
 	}
 	
+	function place_pixels(pixels, incoming){
+		if (!pixels){
+			return;
+		}
+
+		let minX = null;
+		let minY = null;
+		let maxX = null;
+		let maxY = null;
+
+		for (c of Object.keys(pixels)){
+			i = c.split(",").map(Number);
+
+			let c2 = coords_to_center(...i)
+			let k = `${c2[0]},${c2[1]}`
+
+			entire_canvas[k] = pixels[c]
+
+			if (!incoming && room_id){
+				payload.pixels.push([k, pixels[c]])
+			}
+
+			if (i[0] < minX || minX == null){
+				minX = i[0]
+			}
+			if (i[0] > maxX || maxX == null){
+				maxX = i[0]
+			}
+			if (i[1] < minY || minY == null){
+				minY = i[1]
+			}
+			if (i[1] > maxY || maxY == null){
+				maxY = i[1]
+			}
+		}
+
+		minX = Math.max(minX, 0);
+		minY = Math.max(minY, 0);
+		maxX = Math.min(maxX, myCanvas.width)
+		maxY = Math.min(maxY, myCanvas.height)
+
+		
+		let width = maxX - minX;
+		let height = maxY - minY;
+		
+		// console.log(pixels.size, minX, minY, maxX, maxY, width, height)
+
+		if (!width || !height){
+			return;
+		}
+
+		let data = [];
+		
+		for (let y = minY; y < maxY; y++){
+			for (let x = minX; x < maxX; x++){
+				let k = `${x},${y}`;
+				if (pixels.hasOwnProperty(k)){
+					data.push(...[...colors[pixels[k]], 255]);
+				} else {
+					data.push(0, 0, 0, 0)
+				}
+			}
+			
+		}
+		
+		let arr = new Uint8ClampedArray();
+		arr = Uint8ClampedArray.from(data);
+		let imageData = new ImageData(arr, width, height);
+		imageBitmapPromise = createImageBitmap(imageData);
+		Promise.resolve(imageBitmapPromise).then(function(value){
+			ctx.drawImage(value, minX, minY);
+		});
+	}
+
+
 	// Touch Events Handlers
 	draw = {
 		started: false,
-		start: function(evt) {
-
-			ctx.beginPath();
-			ctx.moveTo(
-				evt.touches[0].pageX,
-				evt.touches[0].pageY
-			);
-
+		lastX: null,
+		lastY: null,
+		start: function(e) {
 			this.started = true;
 
+			currX = Math.floor(e.touches[0].pageX - myCanvas.offsetLeft);
+			currY = Math.floor(e.touches[0].pageY - myCanvas.offsetTop);
+			
+			this.lastX = currX;
+			this.lastY = currY;
+
+			c = coords_to_center(currX, currY)
+			payload.x = c[0]
+			payload.y = c[1]
+
+			place_pixels(calculate_line(currX, currY, currX, currY, ctx.lineWidth, stroke_style), false)
+
 		},
-		move: function(evt) {
+		move: function(e) {
+			currX = Math.floor(e.touches[0].pageX - myCanvas.offsetLeft);
+			currY = Math.floor(e.touches[0].pageY - myCanvas.offsetTop);
+			
+			c = coords_to_center(currX, currY)
+			payload.x = c[0]
+			payload.y = c[1]
 
-			if (this.started) {
-				ctx.lineTo(
-					evt.touches[0].pageX,
-					evt.touches[0].pageY
-				);
+			if(this.started !== false) {
 
-				ctx.strokeStyle = stroke_style;
-				ctx.lineWidth = line_width;
-				ctx.stroke();
+				prevX = this.lastX
+				prevY = this.lastY
+				
+				this.lastX = currX;
+				this.lastY = currY;
+
+				// console.log(prevX, prevY, currX, currY)
+
+				place_pixels(calculate_line(prevX, prevY, currX, currY, ctx.lineWidth, stroke_style), false)
+
 			}
-
 		},
-		end: function(evt) {
+		end: function(e) {
 			this.started = false;
 		}
 	};
 
 	function onColorClick(color) {
-		ctx.closePath();
-    	ctx.beginPath();
-		stroke_style = color;
+		stroke_style = Number(color.attr("data-color"));
+		payload.color = stroke_style
 
-    	var borderColor = 'white';
-    	if (color == 'white' || color == 'yellow') {
-        borderColor = 'black';
-    	}
-    	$('#' + lastColor).css("border", "0px dashed white");
-    	$('#' + color).css("border", "1px dashed " + borderColor);
-
-    	lastColor = color;
+		$("#colorToolbar div").removeClass("selected");
+		color.addClass("selected");
     	
 	}
 	
-	function getMousePos(canvas, evt) {
-		var rect = canvas.getBoundingClientRect();
-		return {
-		x: evt.clientX - rect.left,
-		y: evt.clientY - rect.top
-		};
+	function clear_canvas(){
+		ctx.fillStyle = "#fff"
+		ctx.fillRect(0,0, myCanvas.width, myCanvas.height);
+		entire_canvas = {};
+		
 	}
 	
 	// Touch Events
@@ -124,17 +504,71 @@ window.onload = function() {
 	myCanvas.addEventListener('touchend', draw.end, false);
 	myCanvas.addEventListener('touchmove', draw.move, false);
 	
-	// Color toolabar click
-	$('#colorToolbar div').click(function(evt) {
-	console.log(evt) 
-     onColorClick(evt.target.id)});
+	clear_canvas();
 
-	$('#eraser').click(function(evt){
-		ctx.clearRect(0,0, myCanvas.width, myCanvas.height)
+	// Color toolabar click
+	$('#colorToolbar div').click(function(event) {
+    	onColorClick($(event.target))});
+	$('#eraser').click(function (e) {
+		payload.clear = true;
+		clear_canvas();
 	});
+
+	$("#save").click(function(event) {
+		myCanvas.toBlob(function(blob) {
+		saveAs(blob, "whiteboard.png");
+	});
+	
+});
+
+	$('#modalName').on('show.bs.modal', function (event) {
+		$("#nickname-box").val("").attr("placeholder", nickname);
+		$("#nickname-feedback").text("\u{200B}")
+	})
+	$('#modalName').on('shown.bs.modal', function (event) {
+		$("#nickname-box").focus();
+	})
+
+	$("#nickname-form").submit(function (event){
+		event.preventDefault();
+		n = $("#nickname-box").val();
+		if (n){
+			nickname = n
+			payload.nick = n;
+
+			$("#nickname-box").blur();
+			$("#nickname-feedback").text("Your nickname has been set.")
+
+			// I can't close it without bad things happening for whatever reason
+			// so I'm sticking with this (ノಠ益ಠ)ノ
+		}
+	})
+
+	$('#modalJoin').on('show.bs.modal', function (event) {
+		$("#join-box").val("");
+	})
+	$('#modalName').on('shown.bs.modal', function (event) {
+		$("#join-box").focus();
+	})
+
+	$("#join-form").submit(function (event){
+		event.preventDefault();
+		join_id = $("#join-box").val();
+		if (join_id){
+			window.location.replace("http://" + uri + "/" + join_id);
+		}
+	})
+
+	$("#share-button").click(async function(e){
+		data = await fetch("http://" + uri + "/initialize", {method:"POST", body:JSON.stringify(entire_canvas)});
+		room_id = await data.text();
+
+		window.location.replace("http://" + uri + "/" + room_id);
+	})
 
 	// Disable Page Move
 	document.body.addEventListener('touchmove',function(evt){
 		evt.preventDefault();
-	},false);
+	}, {passive: false});
+
 };
