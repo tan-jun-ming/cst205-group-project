5dea9ac04cdffe8d3d362f9a467c2fd6bfd78f0d
tan-jun-ming <jtan@csumb.edu>
2019-12-09 13:47:28 -0800

    Make a prod file thing bc aaaa

---
+++ prod.py
@@ -0,0 +1,8 @@
+from main import app
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(serve(app, Config()))
+
+await serve(app, config)
