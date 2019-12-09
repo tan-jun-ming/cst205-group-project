4ec36118f57be5a11843245523c00ad82953ee39
tan-jun-ming <jtan@csumb.edu>
2019-12-09 14:13:29 -0800

    yeah im sure this will work

--- prod.py
+++ prod.py
@@ -1,5 +1,9 @@
+import sys
 from main import app, loop
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
+
+config = Config()
+config.bind = [sys.argv[1]]
 
 loop.run_until_complete(serve(app, Config()))
